881067a5590f1d48c0b69306785d8ef7d8d1628d
The Nameless One <the.nameless.one@example.com>
2019-05-31 14:32:46 +0000

    -

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
-git_user_name=$(git config user.name)
-git_user_email=$(git config user.email)
 read -e -p "Enter Project Port Prefix (e.g. 401, 402...): " -i 401 port_prefix
 read -e -p "Enter Project Name (folder will be mounted to \"/projects/<name>\"): " project_name
 read -e -p "Bob symbol?" -i "→" bob
-read -e -p "Git user name? " -i "choephix2" git_user_name
-read -e -p "Git user email? " -i "choephix2@gmail.com" git_user_email
+read -e -p "Git user name? " -i $(git config user.name)|'' git_user_name
+read -e -p "Git user email? " -i $(git config user.name)|'' git_user_email
 bob_color=93
 project_path="/projects/$project_name"
 container_name="coder-$project_name"
