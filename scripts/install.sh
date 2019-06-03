04ac1ac2eb3d8ce285aa82e39d1ae6693e203bec
The Nameless One <the.nameless.one@example.com>
2019-06-03 14:56:59 +0000

    install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,7 @@ git_user_name="$(git config user.name || echo '')"
 git_user_email="$(git config user.email || echo '')"
 read -e -p "Enter Project Port Prefix (e.g. 401, 402...): " -i 401 port_prefix
 read -e -p "Enter Project Name (folder will be mounted to \"/projects/<name>\"): " -i test project_name
+read -e -p "Enter Project Password: " password
 read -e -p "Bob symbol?" -i "→" bob
 read -e -p "Git user name? " -i "${git_user_name}" git_user_name
 read -e -p "Git user email? " -i "${git_user_email}" git_user_email
@@ -20,21 +21,19 @@ sudo docker run -dit \
 -e BOB="${bob}" -e BOB_COLOR=${bob_color} \
 -e GIT_USER_EMAIL="${git_user_email}" \
 -e GIT_USER_NAME="${git_user_name}" \
+-e PASSWORD="${password}" \
 -p 0.0.0.0:${port_prefix}00:8443 \
 -p 0.0.0.0:${port_prefix}80:80 \
 -p 0.0.0.0:${port_prefix}81:81 \
 -p 0.0.0.0:${port_prefix}82:82 \
 -p 0.0.0.0:${port_prefix}83:83 \
+-p 0.0.0.0:${port_prefix}88:8080 \
 -p 0.0.0.0:${port_prefix}30:3000 \
 -p 0.0.0.0:${port_prefix}32:5432 \
 -p 0.0.0.0:${port_prefix}36:3306 \
 -p 0.0.0.0:${port_prefix}50:5000 \
--p 0.0.0.0:${port_prefix}88:8080 \
 -p 0.0.0.0:${port_prefix}90:9000 \
--p 0.0.0.0:${port_prefix}91:9001 \
--p 0.0.0.0:${port_prefix}92:9200 \
--p 0.0.0.0:${port_prefix}93:9300 \
 -p 0.0.0.0:${port_prefix}22:22 \
 -v "${project_path}":/workspace \
 -v "${extensions_path}":/vscode/extensions \
-codercom/code-server:latest --allow-http
+choephix2/coder:latest --allow-http
