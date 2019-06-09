919ce81236169bd58c74f3695c4d5775549f2761
choephix2 <46896420+choephix2@users.noreply.github.com>
2019-06-09 19:22:53 +0300

    Update onboot.sh

--- onboot.sh
+++ onboot.sh
@@ -10,6 +10,11 @@ sudo ln -sf /ini/coder/.tmux.conf.local /home/coder/.tmux.conf.local
 sudo ln -sf /ini/coder/.local/share/code-server/User/settings.json /home/coder/.local/share/code-server/User/settings.json
 sudo ln -sf /ini/coder/.local/share/code-server/User/keybindings.json /home/coder/.local/share/code-server/User/keybindings.json
 
+if [[ -ez $PASSWORD ]] 
+  AUTH_OPTION='--no-auth'
+else
+  AUTH_OPTION=''
+
 echo "Staring code-server..."
 code-server --help
 dumb-init code-server --disable-telemetry --allow-http
